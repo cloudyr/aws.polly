8d6576e4638adc98012b70ae20168b684539e208
Thomas J. Leeper <thosjleeper@gmail.com>
2018-04-14 17:13:57 +0100

    cleanup http code & fix session_token arg (closes #2)

--- R/http.R
+++ R/http.R
@@ -2,15 +2,17 @@
 #' @description This is the workhorse function to execute calls to the Polly API.
 #' @param action A character string specifying the API action to take
 #' @param query An optional named list containing query string parameters and their character values.
+#' @param headers A list of headers to pass to the HTTP request.
 #' @param body A request body
 #' @param verb A character string specifying the HTTP verb to implement.
 #' @param version A character string specifying the API version.
 #' @param raw_response A logical indicating whether to return the raw response body.
-#' @param region A character string containing an AWS region. If missing, the default \dQuote{us-east-1} is used.
-#' @param key A character string containing an AWS Access Key ID. The default is pulled from environment variable \dQuote{AWS_ACCESS_KEY_ID}.
-#' @param secret A character string containing an AWS Secret Access Key. The default is pulled from environment variable \dQuote{AWS_SECRET_ACCESS_KEY}.
-#' @param session_token Optionally, a character string containing an AWS temporary Session Token. If missing, defaults to value stored in environment variable \dQuote{AWS_SESSION_TOKEN}.
-#' @param ... Additional arguments passed to \code{\link[httr]{GET}}.
+#' @param verbose A logical indicating whether to be verbose. Default is given by \code{options("verbose")}.
+#' @param region A character string specifying an AWS region. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param key A character string specifying an AWS Access Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param secret A character string specifying an AWS Secret Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param session_token Optionally, a character string specifying an AWS temporary Session Token to use in signing a request. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
 #' @return If successful, a named list. Otherwise, a data structure of class \dQuote{aws-error} containing any error message(s) from AWS and information about the request attempt.
 #' @details This function constructs and signs an Polly API request and returns the results thereof, or relevant debugging information in the case of error.
 #' @author Thomas J. Leeper
@@ -20,23 +22,32 @@
 #' @importFrom aws.signature signature_v4_auth
 #' @export
 pollyHTTP <- 
-function(action, 
-         query = list(),
-         body = NULL,
-         verb = c("GET", "POST", "PUT", "DELETE"),
-         version = "v1",
-         raw_response = if (verb == "POST") TRUE else FALSE,
-         region = NULL, 
-         key = NULL, 
-         secret = NULL, 
-         session_token = NULL,
-         ...) {
+function(
+  action, 
+  query = list(),
+  headers = list(),
+  body = NULL,
+  verb = c("GET", "POST", "PUT", "DELETE"),
+  version = "v1",
+  raw_response = if (verb == "POST") TRUE else FALSE,
+  verbose = getOption("verbose", FALSE),
+  region = Sys.getenv("AWS_DEFAULT_REGION", "us-east-1"), 
+  key = NULL, 
+  secret = NULL, 
+  session_token = NULL,
+  ...
+) {
+    # locate and validate credentials
+    credentials <- locate_credentials(key = key, secret = secret, session_token = session_token, region = region, verbose = verbose)
+    key <- credentials[["key"]]
+    secret <- credentials[["secret"]]
+    session_token <- credentials[["session_token"]]
+    region <- credentials[["region"]]
+    
+    # generate request signature
     verb <- match.arg(verb)
     action <- paste0("/", version, "/", action)
     d_timestamp <- format(Sys.time(), "%Y%m%dT%H%M%SZ", tz = "UTC")
-    if (is.null(region) || region == "") {
-        region <- "us-east-1"
-    }
     url <- paste0("https://polly.",region,".amazonaws.com", action)
     Sig <- signature_v4_auth(
            datetime = d_timestamp,
@@ -47,19 +58,21 @@ function(action,
            query_args = query,
            canonical_headers = list(host = paste0("polly.",region,".amazonaws.com"),
                                     `x-amz-date` = d_timestamp),
-           request_body = if (is.null(body)) "" else toJSON(body, auto_unbox = TRUE),
+           request_body = if (is.null(body)) "" else jsonlite::toJSON(body, auto_unbox = TRUE),
            key = key, 
            secret = secret,
-           session_token = session_token)
-    headers <- list()
+           session_token = session_token,
+           verbose = verbose)
+    # setup request headers
     headers[["x-amz-date"]] <- d_timestamp
     headers[["x-amz-content-sha256"]] <- Sig$BodyHash
+    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     if (!is.null(session_token) && session_token != "") {
         headers[["x-amz-security-token"]] <- session_token
     }
-    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     H <- do.call(add_headers, headers)
-        
+    
+    # execute request
     if (verb == "POST") {
         if (length(query)) {
             r <- POST(url, H, query = query, body = body, encode = "json", ...)
@@ -83,7 +96,7 @@ function(action,
     }
     
     if (http_error(r)) {
-        x <- fromJSON(content(r, "text", encoding = "UTF-8"))
+        x <- jsonlite::fromJSON(content(r, "text", encoding = "UTF-8"))
         warn_for_status(r)
         h <- headers(r)
         out <- structure(x, headers = h, class = "aws_error")
@@ -94,7 +107,7 @@ function(action,
         if (isTRUE(raw_response)) { 
             out <- try(content(r, "raw"))
         } else {
-            out <- try(fromJSON(content(r, "text", encoding = "UTF-8")), silent = TRUE)
+            out <- try(jsonlite::fromJSON(content(r, "text", encoding = "UTF-8")), silent = TRUE)
         }
         if (inherits(out, "try-error")) {
             out <- structure(content(r, "text", encoding = "UTF-8"), "unknown")
